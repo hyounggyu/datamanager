5a30a3ad8d72ad338e5bfe089f84ef15ec76a8c9
Hyounggyu Kim <hgkim10@gmail.com>
2015-06-30 14:28:16 +0900

    Update viewer

--- datamanager/view.py
+++ datamanager/view.py
@@ -1,16 +1,7 @@
+import numpy as np
+import h5py
 from PyQt4 import QtGui
 import pyqtgraph as pg
-
-import numpy as np
-
-
-def swap(im):
-    if im.ndim == 2:
-        return np.swapaxes(im, 0, 1)
-    elif im.ndim == 3:
-        return np.swapaxes(im, 1, 2)
-    else:
-        return None
 
 
 class ViewWindow(QtGui.QMainWindow):
@@ -19,28 +10,26 @@ class ViewWindow(QtGui.QMainWindow):
 
     def __init__(self, parent=None):
         super(ViewWindow, self).__init__(parent)
-
-        filename = QtGui.QFileDialog.getOpenFileName(self, caption='Select file')
-        if filename == '':
-            self.warning('File does not selected')
-            return
-        else:
-            self.openfile(filename)
-            self.initUI()
+        self.open()
 
     def initUI(self):
-        pass
-        # TODO:
-        #imv = pg.ImageView()
-        #imv.setImage(swap(self.image))
-        #self.setCentralWidget(imv)
-        #self.setWindowTitle('ImageView')
-        #self.show()
+        imv = pg.ImageView()
+        imv.setImage(self.image)
+        self.setCentralWidget(imv)
+        self.setWindowTitle('ImageView')
+        self.show()
 
-    def openfile(self):
-        pass
-        # TODO:
-        #self.image = image
+    def open(self):
+        fname = QtGui.QFileDialog.getOpenFileName(self, caption='Select file')
+        if fname == '':
+            self.warning('File does not selected')
+            return
+        f = h5py.File(fname, 'r')
+        dset = f['original/images'] # TODO: selectable
+        arr = np.zeros(dset.shape, dtype=dset.dtype)
+        dset.read_direct(arr)
+        self.image = arr
+        self.initUI()
 
     def warning(self, msg):
         msgbox = QtGui.QMessageBox(self)
