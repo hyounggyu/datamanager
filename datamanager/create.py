18abc7da27fe2d9a19ff31007b209269a5c4df89
Hyounggyu Kim <hgkim10@gmail.com>
2015-07-24 17:37:41 +0900

    Remove almost commands

--- datamanager/create.py
+++ datamanager/create.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 from PyQt4 import QtCore, QtGui
 
-from xni.io import dataset
+from xni.manage import dataset
 
 
 class Worker(QtCore.QObject):
@@ -182,23 +182,6 @@ HDF5 filename: {}'''.format(len(images), len(bgnds), len(darks), os.path.basenam
         msgbox = QtGui.QMessageBox(self)
         msgbox.setText(msg)
         msgbox.exec_()
-
-
-def _findtiff(path, prefix):
-    return sorted([p for p in path.iterdir() if p.match(prefix.strip()+'*') and (p.suffix.lower() in ['.tif', '.tiff'])])
-
-
-def start_create(args):
-    path = Path(args.path)
-    images = _findtiff(path, args.image_prefix)
-    bgnds = _findtiff(path, args.background_prefix) if args.background_prefix != None else []
-    darks = _findtiff(path, args.dark_prefix) if args.dark_prefix != None else []
-    # TODO: dataset.create will accept pathlib
-    images = [str(im) for im in images]
-    bgnds = [str(im) for im in bgnds]
-    darks = [str(im) for im in darks]
-    for i, name in dataset.create(args.output, images, bgnds, darks):
-        print(i, name)
 
 
 def start_createqt(args):
